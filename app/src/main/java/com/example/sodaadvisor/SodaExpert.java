75fa2ecd07cb6fc8df6c701453c701cf82f9dfb2
Katerina Raf <KateSu1989@gmail.com>
2023-06-14 21:41:37 +0500

    First app upgrade

--- app/src/main/java/com/example/sodaadvisor/SodaExpert.java
+++ app/src/main/java/com/example/sodaadvisor/SodaExpert.java
@@ -8,14 +8,22 @@ public class SodaExpert {
     List<String> getBrands(String color) {
         List<String> brands = new ArrayList<>(); // здесь просто объявили нашу коллекцию(коллекция хранит набор каких-то значений)
 
-        if (color.equals("Black")) { // метод equals, который можно вызвать у любой строки, просто сравнивает то, что передаётся ему в аргумент
+        if (color.equals("Green")) { // метод equals, который можно вызвать у любой строки, просто сравнивает то, что передаётся ему в аргумент
+            brands.add("Green Cola");
+            brands.add("GreenMe");
+            brands.add("Mountain Dew");
+        } else if (color.equals("Black")){
             brands.add("Coca-Cola");
             brands.add("Lipton Black");
             brands.add("Everlast Cola");
+        } else if (color.equals("White")) {
+            brands.add("Sprite");
+            brands.add("Bitter Lemon");
+            brands.add("Mohito");
         } else {
             brands.add("Mirinda");
-            brands.add("Lipton Green");
-            brands.add("Sprite");
+            brands.add("Epsa");
+            brands.add("Fentimans Rose Lemonade");
         }
 
         return brands;
